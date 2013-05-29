014c852fd4aaa10ea837ad10e9b128809ccf4e60
hubugui <hubugui@gmail.com>
2013-05-29 17:37:00 +0800

    add yuv size relate macros

--- yuv.h
+++ yuv.h
@@ -5,6 +5,9 @@
 extern "C" {
 #endif
 
+#define uyvy_size(w, h)	(w * h * 2))
+#define i420_size(w, h)	((w * h * 3) / 2)
+
 void uyvy422_to_i420_4byte(const char *src, char *dst, unsigned int w, unsigned int h);
 void uyvy422_to_i420_wh(const char *src, char *dst, unsigned int w, unsigned int h);
 
